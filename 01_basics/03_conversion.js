ea5fa1065713bfd95ed2c0d327cd3e02f93f7ac6
Archit-Verma-2000 <143899126+Archit-Verma-2000@users.noreply.github.com>
2024-07-29 12:59:22 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+let score=false
+//console.log(typeof score)
+
+let valueInNumber=Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+// "33" =>334
+//"33abc"=>NaN
+//true=>1;false=>0
+let isLoggedIn="HITESH"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
