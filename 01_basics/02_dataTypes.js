59ed8c232d19a050ecf188f5f4a58ad442ac4170
Archit-Verma-2000 <143899126+Archit-Verma-2000@users.noreply.github.com>
2024-07-29 12:19:14 +0000

    datatypes saved

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";
+//alert(3+3)//we are using nodejs not browser
+console.log(3
+    +
+3)//code readability is imp.
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+//number=>2 to power 53
+//bigint
+//string => ""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof "hitesh")
+console.log(typeof null)
+console.log(typeof undefined)
